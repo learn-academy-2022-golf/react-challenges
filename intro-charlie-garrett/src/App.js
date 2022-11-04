a5def4e190f6d99c9678531facb76297ac8aba82
gmgreer <beauski8@gmail.com>
2022-11-04 11:50:58 -0700

    firt commit

---
+++ intro-charlie-garrett/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Profile />
+      <Favorites />
+   </>   
+   
+  );
+}
+
+export default App;
---
+++ intro-charlie-garrett/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+       <>
+         <h1>Learn Academy Cohort</h1>
+       </> 
+    )
+}
+
+export default Header
