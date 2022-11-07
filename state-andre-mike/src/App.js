2413d18fb81361e6eadfe6f466cd3d1b4246987b
MikeSC27 <mcruzen89@gmail.com>
2022-11-07 14:39:22 -0800

    color box just has a title

---
+++ state-andre-mike/src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <>
+      <h1>Color Box</h1>
+      <p>Color: </p>
+    </>
+  )
+
+}
+
+const background = () => {
+  return (
+   <>
+
+  background-color: #20438a;
+  color: rgb(223, 36, 36);
+  padding: 40px;
+  font-family: Sans-Serif;
+  text-align: center;
+
+   </>
+  )
+
+}
+
+
+export default App
