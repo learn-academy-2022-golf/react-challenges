f2c0f2dffc5f06f0480039fb6c0b3df519526d9c
cibrown94 <cianibrown94@aol.com>
2022-11-08 09:43:35 -0800

    we tried

---
+++ props-cb-me/src/components.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const roll = (props) => {
+    return(
+      <>
+        
+        <p onClick= {props.roll}>{props.roll[roll.index]}</p>
+      </>
+    )
+  }
+
+
+
+
+
+export default App
+
+{/* <p  onClick ={}></p> */}
