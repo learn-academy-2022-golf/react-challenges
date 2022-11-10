005d593ef6f33566e14c8eedd850aa6053238b50
TKash89 <tyler.kasheta@outlook.com>
2022-11-10 12:04:14 -0800

    very close to solving BADBOT

--- events-tyler-blake/src/App.js
+++ events-tyler-blake/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from "react"
 import Goodbot from './components/Goodbot';
+import Badbot from './components/BadBot';
 
 const App = () => {
   const [userInput, setUserInput] = useState("")
@@ -14,6 +15,8 @@ const App = () => {
     <h2>Type something here!</h2>
     <input type="text" value={userInput} onChange={handleChange} />
     <Goodbot userInput={userInput}/>
+    <Badbot userInput={userInput}/>
+
     </>
   );
 }
