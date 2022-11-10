59c81ff9e0eeb83990bc041370e6d88626d3fb11
matt92101 <matt92101@hotmail.com>
2022-11-10 12:03:38 -0800

    Trying to get MaloBot to say Blah Blah

---
+++ inputs-matt-zeke-tabi/src/components/MaloBot.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MaloBot = (props) => {
+    
+  return (
+    <>
+    <h2>MaloBot</h2>
+    <p>I hear you saying {props.text}. Is this correct?</p>
+    
+    </>
+  )
+  
+}
+
+export default MaloBot
